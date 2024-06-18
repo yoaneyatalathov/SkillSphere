b15c7315caedd077f3402c340caa57f3dcc0ba4f
Fadhil Faqih <153825017+Faqyh@users.noreply.github.com>
2024-06-18 17:49:20 +0800

    menambahkan logika logout

--- script/user/detail.js
+++ script/user/detail.js
@@ -1,7 +1,7 @@
-import { checkLoginAndUser } from "./authUtil.js";
+import { checkLoginAndUser, handleLogout } from "./authUtil.js";
 
 const urlSearchParams = new URLSearchParams(window.location.search);
-const id = urlSearchParams.get('id');
+const id = urlSearchParams.get("id");
 
 document.addEventListener("DOMContentLoaded", async () => {
   const detailItem = document.getElementById("detail-item");
@@ -10,15 +10,15 @@ document.addEventListener("DOMContentLoaded", async () => {
     await checkLoginAndUser();
 
     const response = await fetch(`http://localhost:3000/trainings/${id}`);
-    
-    console.log(response)
+
+    console.log(response);
     // Periksa jika respons berhasil dan dalam format JSON
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
     }
 
     const training = await response.json();
-    console.log(training)
+    console.log(training);
 
     // Tampilkan data pelatihan
     const trainingItem = document.createElement("div");
@@ -29,23 +29,48 @@ document.addEventListener("DOMContentLoaded", async () => {
       </div>
       <div class="card-body">
         <h3 class="card-title text-center">${training.nama_pelatihan}</h3>
-        <h4 class="card-subtitle mb-2 fw-light text-center">${training.penyelenggara}</h4>
-        <p class="card-text"><strong>Durasi:</strong> ${training.durasi} Minggu</p>
-        <p class="card-text"><strong>Kelompok Umur:</strong> ${training.kelompok_umur} Tahun</p>
+        <h4 class="card-subtitle mb-2 fw-light text-center">${
+          training.penyelenggara
+        }</h4>
+        <p class="card-text"><strong>Durasi:</strong> ${
+          training.durasi
+        } Minggu</p>
+        <p class="card-text"><strong>Kelompok Umur:</strong> ${
+          training.kelompok_umur
+        } Tahun</p>
         <p class="card-text"><strong>Kategori:</strong> ${training.kategori}</p>
-        <p class="card-text"><strong>Deskripsi:</strong> ${training.deskripsi}</p>
+        <p class="card-text"><strong>Deskripsi:</strong> ${
+          training.deskripsi
+        }</p>
         <h5>Persyaratan:</h5>
-        <ul>${training.persyaratan.map(item => `<li>${item}</li>`).join('')}</ul>
+        <ul>${training.persyaratan
+          .map((item) => `<li>${item}</li>`)
+          .join("")}</ul>
         <h5>Fasilitas:</h5>
-        <ul>${training.fasilitas.map(item => `<li>${item}</li>`).join('')}</ul>
-        <p class="card-text"><strong>Pendaftaran:</strong> ${training.pendaftaran}</p>
+        <ul>${training.fasilitas
+          .map((item) => `<li>${item}</li>`)
+          .join("")}</ul>
+        <p class="card-text"><strong>Pendaftaran:</strong> ${
+          training.pendaftaran
+        }</p>
         <p class="card-text"><strong>Kontak:</strong></p>
-        <ul>${training.kontak.map(item => `<li>${item}</li>`).join('')}</ul>
-        <p class="card-text"><strong>Link Pendaftaran:</strong> <a href="${training.link_pendaftaran}" target="_blank">${training.link_pendaftaran}</a></p>
-        <p class="card-text"><strong>Sumber Informasi:</strong> <a href="${training.sumber_informasi}" target="_blank">${training.sumber_informasi}</a></p>
+        <ul>${training.kontak.map((item) => `<li>${item}</li>`).join("")}</ul>
+        <p class="card-text"><strong>Link Pendaftaran:</strong> <a href="${
+          training.link_pendaftaran
+        }" target="_blank">${training.link_pendaftaran}</a></p>
+        <p class="card-text"><strong>Sumber Informasi:</strong> <a href="${
+          training.sumber_informasi
+        }" target="_blank">${training.sumber_informasi}</a></p>
       </div>
     `;
     detailItem.appendChild(trainingItem);
+
+    // Tambahkan event listener untuk link logout
+    const logoutLink = document.getElementById("logoutLink");
+    logoutLink.addEventListener("click", function (event) {
+      event.preventDefault(); // Mencegah link untuk berpindah ke halaman lain
+      handleLogout(); // Panggil fungsi handleLogout saat link logout ditekan
+    });
 
   } catch (error) {
     console.error("Error fetching trainings:", error);
