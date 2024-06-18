b4497818b0aa023ff6e85f5529e1bb6ad3e6390b
Fadhil Faqih <153825017+Faqyh@users.noreply.github.com>
2024-06-18 14:20:33 +0800

    mengganti tanda kutip

--- script/user/authUtil.js
+++ script/user/authUtil.js
@@ -6,30 +6,30 @@ const SESSION_TIMEOUT_HOURS = 3; // Waktu timeout session dalam jam
 
 // Fungsi untuk mengatur session timeout
 function setSessionTimeout() {
-    setTimeout(() => {
-        logout(); // Panggil fungsi logout saat session timeout
-        window.location.href = "login-page.html"; // Redirect ke halaman login
-    }, SESSION_TIMEOUT_HOURS * 60 * 60 * 1000); // Konversi jam ke milidetik
+  setTimeout(() => {
+    logout(); // Panggil fungsi logout saat session timeout
+    window.location.href = "login-page.html"; // Redirect ke halaman login
+  }, SESSION_TIMEOUT_HOURS * 60 * 60 * 1000); // Konversi jam ke milidetik
 }
 
 // Fungsi untuk memeriksa login dan user
 export async function checkLoginAndUser() {
-    if (!isLoggedIn()) {
-        // Jika tidak ada pengguna yang login, redirect kembali ke halaman login.html
-        window.location.href = "login-page.html";
-        throw new Error('User is not logged in');
-    }
+  if (!isLoggedIn()) {
+    // Jika tidak ada pengguna yang login, redirect kembali ke halaman login.html
+    window.location.href = "login-page.html";
+    throw new Error("User is not logged in");
+  }
 
-    const loggedInUser = getLoggedInUser();
-    console.log(loggedInUser);
+  const loggedInUser = getLoggedInUser();
+  console.log(loggedInUser);
 
-    // Set session timeout
-    setSessionTimeout();
+  // Set session timeout
+  setSessionTimeout();
 
-    return loggedInUser;
+  return loggedInUser;
 }
 
 export function handleLogout() {
-    logout(); // Panggil fungsi logout
-    window.location.href = "login-page.html"; // Redirect ke halaman login
+  logout(); // Panggil fungsi logout
+  window.location.href = "login-page.html"; // Redirect ke halaman login
 }
